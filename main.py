e337c3cfeed96dad2a5be645f75cc26c2bab123d
Daniil Roman <danroman17397@gmail.com>
2022-12-03 13:47:08 +0100

    add several chat ids support

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tgtg import TgtgClient
 from src.dynamodb import DynamodbConfig, ItemStoreService
 from src.items_getter import get_items_str
 
-TELEGRAM_CHAT_ID = config("TELEGRAM_CHAT_ID")
+TELEGRAM_CHAT_IDS = config("TELEGRAM_CHAT_ID").split(",")
 TELEGRAM_TOKEN = config("TELEGRAM_TOKEN")
 
 TGTG_ACCESS_TOKEN = config("TGTG_ACCESS_TOKEN")
@@ -16,13 +16,13 @@ TGTG_USER_ID = config("TGTG_USER_ID")
 
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-REGION_NAME = config("AWS_REGION_NAME")
+AWS_REGION_NAME = config("AWS_REGION_NAME")
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
                     filemode="w")
 
-dynamodb_config = DynamodbConfig(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME)
+dynamodb_config = DynamodbConfig(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION_NAME)
 item_store_service = ItemStoreService(dynamodb_config)
 
 tgtg_client = TgtgClient(
@@ -44,7 +44,8 @@ def __send_telegram_message(msg):
     else:
         bot = telepot.Bot(TELEGRAM_TOKEN)
         bot.getMe()
-        bot.sendMessage(TELEGRAM_CHAT_ID, msg)
+        for chat_id in TELEGRAM_CHAT_IDS:
+            bot.sendMessage(chat_id, msg)
 
 
 if __name__ == '__main__':
